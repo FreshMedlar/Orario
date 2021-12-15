abdf40245f41aa00c53f53a6c7cd4dbc5bccbff3
FreshMedlar <emaxtiger3000@gmail.com>
2021-12-15 13:45:53 +0100

    First commit

--- app/src/main/java/com/example/orario/MainActivity.kt
+++ app/src/main/java/com/example/orario/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.orario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.SyncStateContract
-import androidx.core.content.ContentProviderCompat.requireContext
-import java.io.File
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,39 +11,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    private fun getExcelFile(): File?{
-        //Get App Director, APP_DIRECTORY_NAME is a string
-        val appDirectory = requireContext().getExternalFilesDir(SyncStateContract.Constants.APP_DIRECTORY_NAME)
-
-        //Check App Directory whether it exists or not
-        appDirectory?.let {
-
-            //Check if file exists or not
-            if (it.exists()) {
-
-                //return excel file
-                return File(appDirectory, SyncStateContract.Constants.FILE_NAME)
-            }
-        }
-        return null
-    }
-
-    private fun readExcelAsWorkbook(): Workbook?{
-
-        //Reading excel file
-        getExcelFile()?.let {
-            try {
-
-                //Reading excel file as stream
-                val inputStream = FileInputStream(it)
-
-                //Return workbook
-                return WorkbookFactory.create(inputStream)
-            } catch (ex: Exception) {
-                ex.printStackTrace()
-            }
-        }
-
-        return null
-    }
 }
